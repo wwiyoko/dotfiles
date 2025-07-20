b13a85a223e56963df070c5ebfca80842e76e984
Wahyu Wiyoko <wwiyoko@protonmail.com>
2025-07-20 13:07:29 +0800

    Simplify a few configuration
    - Remove some environment variables.
    - Remove indentation in Git configuration.
    - Remove `relativenumber` and `list` option in Neovim.
    - Use `Mod4` directly and remove less used Sway keybind.
    - Extend history limit and add a few Vim-like keybind in tmux.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,8 @@
 vim.o.clipboard = "unnamedplus"
-vim.o.relativenumber = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.list = true
 vim.o.undofile = true
 
 vim.api.nvim_create_autocmd("FileType", {
